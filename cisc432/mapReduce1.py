2d22353aae5ba7ce31eb3c67cca1979608d1f7f7
Mohamed Sami Rakha <10222450+codemiles@users.noreply.github.com>
2018-10-17 22:00:02 -0700

    Add files via upload

--- cisc432/mapReduce1.py
+++ cisc432/mapReduce1.py
@@ -10,7 +10,7 @@
 #data1: wget http://mohamedsamirakha.info/cisc432/ratings.dat
 #MapReduce1 wget http://mohamedsamirakha.info/cisc432/mapReduce1.py
 #Run on hadoop
-#test if it is working locally: python 
+#test if it is working locally: python mapReduce1.py ./ratings.dat
 #python mapReduce1.py -r hadoop --hadoop-streaming-jar/usr/hdp/current/hadoop-mapreduce-clienthadoop-streaming.jar rating.dat
 from mrjob.job import MRJob 
 from mrjob.step import MRStep
@@ -20,6 +20,7 @@ class MoviesReviewsCount(MRJob):
            return [ 
                 MRStep(mapper=self.mapper_get_movies,	
                      reducer=self.reducer_count_reviews)
+                  ]
     #Mapping Function
     def mapper_get_ratings(self,_,line):
          (userId, movieId rating, timestamp) = line.split(',')

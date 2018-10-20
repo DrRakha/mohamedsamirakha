7c3fffb174211361bacc82616f39670b2bea5fd9
Mohamed Sami Rakha <10222450+codemiles@users.noreply.github.com>
2018-10-20 16:32:50 -0700

    Add files via upload

--- cisc432/mapReduce1.py
+++ cisc432/mapReduce1.py
@@ -2,16 +2,16 @@
 # Credits: M. S. Rakha, Ph.D., Queen\'s University, CISC/CMPE 432
 # HortonWorks SandBox with HDP 2.6.4
 # Connect SSH IP Port=2222 (Using putty)
-# Switch to root
+# Switch to root : sudo -i
+#yum install nano [editor] optional
 #yum install python-pip
 #pip install google-api-python-client==1.6.4
 #pip install mrjob==0.5.11
-#yum install nano [editor] optional
 #data1: wget http://mohamedsamirakha.info/cisc432/mapReduceData.dat
 #MapReduce1 wget http://mohamedsamirakha.info/cisc432/mapReduce1.py
 #Run on hadoop
 #test if it is working locally: python mapReduce1.py ./mapReduceData.dat
-#python mapReduce1.py -r hadoop --hadoop-streaming-jar /usr/hdp/current/hadoop-mapreduce-clienthadoop-streaming.jar mapReduceData.dat
+#python mapReduce1.py -r hadoop --hadoop-streaming-jar /usr/hdp/current/hadoop-mapreduce-client/hadoop-streaming.jar ./mapReduceData.dat
 from mrjob.job import MRJob 
 from mrjob.step import MRStep
 
@@ -32,4 +32,8 @@ class MoviesReviewsCount(MRJob):
          yield key, sum(values)
 
 if __name__ == '__main__':
+    start_time = datetime.now()
     MoviesReviewsCount.run()
+	end_time = datetime.now()
+    elapsed_time = end_time - start_time
+    sys.stderr.write(elapsed_time)

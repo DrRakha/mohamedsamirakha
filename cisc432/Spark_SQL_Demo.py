6046c068ce6650efbf935107c6ae285ca2a20184
Mohamed Sami Rakha <10222450+codemiles@users.noreply.github.com>
2018-10-23 16:57:22 -0700

    Add files via upload

--- cisc432/Spark_SQL_Demo.py
+++ cisc432/Spark_SQL_Demo.py
@@ -37,8 +37,12 @@ if __name__ == "__main__":
     moviesDataFrame = sc.createDataFrame(movieRatings)
 	 # Count the number of reviews, which also equal to the number of movie rows, add it to a new DataFrame called reviewCounts
     reviewCounts = moviesDataFrame.groupBy("itemId").count()
-     # Pull the top 10 results
-    limitMovies = reviewCounts.orderBy("count()").take(10)
+     # Pull the top 5  results
+	for movie in reviewCounts.take(5)
+        print (movie[1], movie[2])
+	 
+	    # Pull the top 5  results
+    limitMovies = reviewCounts.orderBy("count").take(10)
     # Print them out, converting movie ID's to names as we go.
     for movie in limitMovies:
         print (movie[1], movie[2])
